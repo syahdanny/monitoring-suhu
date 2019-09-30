67e923c19d00676288e5ae87df4a6ef3f24222f8
syahdanny <syahdanny11@gmail.com>
2019-09-30 09:05:05 +0700

    splash screen

---
+++ app/src/main/java/com/mmdiyul/monitoringsuhu/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.mmdiyul.monitoringsuhu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+    }
+}
+
