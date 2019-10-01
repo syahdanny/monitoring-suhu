1077ea4c9d5358265f6df8713f80b0a97018b644
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2019-10-01 08:53:08 +0700

    edit layout + greetings

--- app/src/main/java/com/mmdiyul/monitoringsuhu/MainActivity.java
+++ app/src/main/java/com/mmdiyul/monitoringsuhu/MainActivity.java
@@ -1,14 +1,34 @@
 package com.mmdiyul.monitoringsuhu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView greetings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ucapan selamat berdasarkan waktu.
+        greetings = findViewById(R.id.greetings);
+
+        int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        if (currentHour < 10) {
+            greetings.setText("Selamat Pagi!");
+        } else if (currentHour < 15) {
+            greetings.setText("Selamat Siang!");
+        } else if (currentHour < 18) {
+            greetings.setText("Selamat Sore!");
+        } else {
+            greetings.setText("Selamat Malam!");
+        }
     }
 }
