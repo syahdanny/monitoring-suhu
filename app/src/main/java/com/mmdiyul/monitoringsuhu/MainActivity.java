268309f4806240338664e8e82c96582be335f135
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2019-10-07 10:04:29 +0700

    get current data from firebase

--- app/src/main/java/com/mmdiyul/monitoringsuhu/MainActivity.java
+++ app/src/main/java/com/mmdiyul/monitoringsuhu/MainActivity.java
@@ -6,20 +6,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView greetings;
+    private TextView textSuhu;
+    private TextView textKelembaban;
+
+    private DatabaseReference databaseReference;
+
+    private String suhu;
+    private String kelembaban;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // ucapan selamat berdasarkan waktu.
+        // bind
         greetings = findViewById(R.id.greetings);
+        textSuhu = findViewById(R.id.textSuhu);
+        textKelembaban = findViewById(R.id.textKelembaban);
 
+        // ucapan selamat berdasarkan waktu.
         int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
         if (currentHour < 10) {
             greetings.setText("Selamat Pagi!");
@@ -30,5 +47,26 @@ public class MainActivity extends AppCompatActivity {
         } else {
             greetings.setText("Selamat Malam!");
         }
+
+        // Database
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        Query lastData = databaseReference.child("sensor");
+        lastData.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                long lengthData = dataSnapshot.getChildrenCount();
+                long lastChild = lengthData - 1;
+                suhu = dataSnapshot.child(lastChild + "/suhu").getValue().toString();
+                kelembaban = dataSnapshot.child(lastChild + "/kelembaban").getValue().toString();
+
+                textSuhu.setText(suhu + " C");
+                textKelembaban.setText(kelembaban + " %");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
