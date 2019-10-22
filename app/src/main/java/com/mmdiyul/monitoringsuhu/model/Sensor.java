c2a8626ee297de345e94dcfdb545822540fb5064
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2019-10-22 16:40:56 +0700

    fix constructor

--- app/src/main/java/com/mmdiyul/monitoringsuhu/model/Sensor.java
+++ app/src/main/java/com/mmdiyul/monitoringsuhu/model/Sensor.java
@@ -2,14 +2,14 @@ package com.mmdiyul.monitoringsuhu.model;
 
 public class Sensor {
     private int id;
-    private double kelembaban;
     private double suhu;
+    private double kelembaban;
     private String update;
 
-    public Sensor(int id, double kelembaban, double suhu, String update) {
+    public Sensor(int id, double suhu, double kelembaban, String update) {
         this.id = id;
-        this.kelembaban = kelembaban;
         this.suhu = suhu;
+        this.kelembaban = kelembaban;
         this.update = update;
     }
 
